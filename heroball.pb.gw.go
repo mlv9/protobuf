917d291a5a9d6c4b237c965250cf3e8e11580125
Matt <readspaceapp@gmail.com>
2019-04-23 23:52:41 +1000

    Updated protobuf

--- heroball.pb.gw.go
+++ heroball.pb.gw.go
@@ -41,6 +41,19 @@ func request_HeroBallService_GetPlayerInfo_0(ctx context.Context, marshaler runt
 
 }
 
+func request_HeroBallService_GetTeamInfo_0(ctx context.Context, marshaler runtime.Marshaler, client HeroBallServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetTeamInfoRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetTeamInfo(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_HeroBallService_GetGameInfo_0(ctx context.Context, marshaler runtime.Marshaler, client HeroBallServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetGameInfoRequest
 	var metadata runtime.ServerMetadata
@@ -134,6 +147,35 @@ func RegisterHeroBallServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("POST", pattern_HeroBallService_GetTeamInfo_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_HeroBallService_GetTeamInfo_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HeroBallService_GetTeamInfo_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_HeroBallService_GetGameInfo_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -198,6 +240,8 @@ func RegisterHeroBallServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 var (
 	pattern_HeroBallService_GetPlayerInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "get", "player", "info"}, ""))
 
+	pattern_HeroBallService_GetTeamInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "get", "team", "info"}, ""))
+
 	pattern_HeroBallService_GetGameInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "get", "game", "info"}, ""))
 
 	pattern_HeroBallService_GetCompetitionInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "get", "competition", "info"}, ""))
@@ -205,6 +249,8 @@ var (
 
 var (
 	forward_HeroBallService_GetPlayerInfo_0 = runtime.ForwardResponseMessage
+
+	forward_HeroBallService_GetTeamInfo_0 = runtime.ForwardResponseMessage
 
 	forward_HeroBallService_GetGameInfo_0 = runtime.ForwardResponseMessage
 
